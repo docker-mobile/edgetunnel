6fa238c7b7ee03a5047b8e83936aaf910992349a
docker-mobile <143889618+docker-mobile@users.noreply.github.com>
2024-05-22 17:27:39 +0330

    Update cf-proxy.js

--- client-config/cf-proxy.js
+++ client-config/cf-proxy.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
         let url = new URL(request.url);
-        url.hostname = ''
+        url.hostname = 'wiki.wesalpixel.ir'
         let new_request = new Request(url, request);
         console.log('-----', new_request.url)
         return fetch(new_request);
